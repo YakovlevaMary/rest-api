34afb2eccc6de35e15822891aa217a9595f3eac3
MaryPimenova <alla.pim-wolf@mail.ru>
2023-06-06 13:57:09 +0300

    Removed browser configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api/";
     }
