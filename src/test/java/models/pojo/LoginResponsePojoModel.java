e7298ac7c14a3cbb91085dc135a32ccf1e758817
MaryPimenova <alla.pim-wolf@mail.ru>
2023-06-16 16:05:06 +0300

    Initial commit

---
+++ src/test/java/models/pojo/LoginResponsePojoModel.java
@@ -0,0 +1,19 @@
+package models.pojo;
+
+public class LoginResponsePojoModel {
+    /*
+    {
+        "token": "QpwL5tke4Pnpja7X4"
+    }
+     */
+
+    String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/test/java/specs/LoginSpecs.java
@@ -0,0 +1,30 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class LoginSpecs {
+    public static RequestSpecification loginRequestSpec = with()
+            .log().uri()
+            .log().body()
+            .filter(withCustomTemplates())
+            .contentType(JSON)
+            .baseUri("https://reqres.in")
+            .basePath("/api");
+
+    public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody("token", notNullValue())
+            .build();
+
+}
